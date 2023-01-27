4443ce6c8a78c80811eacc2b22f1f8c04548d803
dolpongg <dolpong_@naver.com>
2023-01-27 10:10:24 +0900

    작동됨

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // local vue api axios instance
 function apiInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:9999/",
+    baseURL: "http://localhost:3000",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
